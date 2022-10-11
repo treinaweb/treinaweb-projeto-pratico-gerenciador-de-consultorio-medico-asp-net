fda2ab683cacb8638887446b12602686e60d82dd
Joel Rodrigues <joelrlneto@gmail.com>
2022-10-11 17:11:16 -0300

    03-04-Carregando a string de conexão do appsettings

--- Models/Contexts/SisMedContext.cs
+++ Models/Contexts/SisMedContext.cs
@@ -6,11 +6,17 @@ namespace SisMed.Models.Contexts
 {
     public class SisMedContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+
+        public SisMedContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public DbSet<Medico> Medicos => Set<Medico>();
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=Localhost\\SQLExpress;Database=SisMed;Integrated Security=True;");
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("SisMed"));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
