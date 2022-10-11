c78dfa3f3158d1212487045b425150efce1c9244
Joel Rodrigues <joelrlneto@gmail.com>
2022-10-11 17:15:21 -0300

    03-05-Adicionando a primeira migration

---
+++ Migrations/20221011201300_EntidadeMedico.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SisMed.Migrations
+{
+    public partial class EntidadeMedico : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Medicos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CRM = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicos", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medicos_CRM",
+                table: "Medicos",
+                column: "CRM",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medicos");
+        }
+    }
+}
