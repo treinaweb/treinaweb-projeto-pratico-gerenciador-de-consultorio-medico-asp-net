b8ada67ff07045046dd232da2149fc00e088dffb
Joel Rodrigues <joelrlneto@gmail.com>
2022-10-11 17:03:51 -0300

    03-01-Criando a entidade Medico

---
+++ Models/Entities/Medico.cs
@@ -0,0 +1,9 @@
+namespace SisMed.Models.Entities
+{
+    public class Medico
+    {
+        public int Id { get; set; }
+        public string CRM { get; set; } = string.Empty;
+        public string Nome { get; set; } = string.Empty;
+    }
+}
