565b5f0f921a199354ea49c3d19b6e3fe31e4537
Joel Rodrigues <joelrlneto@gmail.com>
2022-10-16 19:57:10 -0300

    04-03-Implementando a listagem de pacientes

---
+++ Controllers/PacientesController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using SisMed.Models.Contexts;
+using SisMed.ViewModels.Pacientes;
+
+namespace SisMed.Controllers
+{
+    public class PacientesController : Controller
+    {
+        private readonly SisMedContext _context;
+        private const int TAMANHO_PAGINA = 10;
+
+        public PacientesController(SisMedContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(string filtro, int pagina = 1)
+        {
+            var medicos = _context.Pacientes.Where(x => x.Nome.Contains(filtro) || x.CPF.Contains(filtro))
+                                            .Select(x => new ListarPacienteViewModel
+                                            {
+                                              Id = x.Id,
+                                              CPF = x.CPF,
+                                              Nome = x.Nome
+                                            });
+
+            ViewBag.Filtro = filtro;
+            ViewBag.NumeroPagina = pagina;
+            ViewBag.TotalPaginas = Math.Ceiling((decimal)medicos.Count() / TAMANHO_PAGINA);
+            return View(medicos.Skip((pagina - 1) * TAMANHO_PAGINA).Take(TAMANHO_PAGINA));
+        }
+    }
+}
---
+++ ViewModels/Pacientes/ListarPacienteViewModel.cs
@@ -0,0 +1,9 @@
+namespace SisMed.ViewModels.Pacientes
+{
+    public class ListarPacienteViewModel
+    {
+        public int Id { get; set; }
+        public string CPF { get; set; } = string.Empty;
+        public string Nome { get; set; } = string.Empty;
+    }
+}
