787175516906410c8d97f37672823df093409390
Joel Rodrigues <joelrlneto@gmail.com>
2022-10-16 06:24:40 -0300

    03-10-Adicionando a página de inserção de médicos

--- Controllers/MedicosController.cs
+++ Controllers/MedicosController.cs
@@ -14,7 +14,7 @@ namespace SisMed.Controllers
             _context = context;
         }
 
-         public IActionResult Index(string filtro, int pagina = 1)
+        public IActionResult Index(string filtro, int pagina = 1)
         {
             var medicos = _context.Medicos.Where(x => x.Nome.Contains(filtro) || x.CRM.Contains(filtro))
                                           .Select(x => new ListarMedicoViewModel
@@ -28,6 +28,11 @@ namespace SisMed.Controllers
             ViewBag.NumeroPagina = pagina;
             ViewBag.TotalPaginas = Math.Ceiling((decimal)medicos.Count() / TAMANHO_PAGINA);
             return View(medicos.Skip((pagina - 1) * TAMANHO_PAGINA).Take(TAMANHO_PAGINA));
+        }
+
+        public IActionResult Adicionar()
+        {
+            return View();
         }
     }
 }
---
+++ ViewModels/Medicos/AdicionarMedicoViewModel.cs
@@ -0,0 +1,8 @@
+namespace SisMed.ViewModels.Medicos
+{
+    public class AdicionarMedicoViewModel
+    {
+        public string CRM { get; set; } = string.Empty;
+        public string Nome { get; set; } = string.Empty;
+    }
+}
