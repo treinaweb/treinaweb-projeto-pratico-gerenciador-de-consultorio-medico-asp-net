dcbd74f45b676da92fda86244b82adc4dc0b5d7a
Joel Rodrigues <joelrlneto@gmail.com>
2022-10-29 21:19:09 -0300

    07-04-Implementando a listagem de consultas

--- Controllers/ConsultasController.cs
+++ Controllers/ConsultasController.cs
@@ -1,18 +1,38 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using SisMed.Models.Contexts;
+using SisMed.Models.Enums;
+using SisMed.ViewModels.Consultas;
 
 namespace SisMed.Controllers
 {
     public class ConsultasController : Controller
     {
-        public IActionResult Index()
+        private readonly SisMedContext _context;
+        private const int TAMANHO_PAGINA = 10;
+
+        public ConsultasController(SisMedContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public IActionResult Index(string filtro, int pagina = 1)
+        {
+            var consultas = _context.Consultas.Include(c => c.Paciente)
+                                              .Include(c => c.Medico)
+                                              .Where(c => c.Paciente.Nome.Contains(filtro) || c.Medico.Nome.Contains(filtro))
+                                              .Select(c => new ListarConsultaViewModel
+                                              {
+                                                Id = c.Id,
+                                                Paciente = c.Paciente.Nome,
+                                                Medico = c.Medico.Nome,
+                                                Data = c.Data,
+                                                Tipo = c.Tipo == TipoConsulta.Eletiva ? "Eletiva" : "Urgência"
+                                              });
+            ViewBag.NumeroPagina = pagina;
+            ViewBag.TotalPaginas = Math.Ceiling((decimal)consultas.Count() / TAMANHO_PAGINA);
+
+            return View(consultas.Skip((pagina - 1) * TAMANHO_PAGINA).Take(TAMANHO_PAGINA));
         }
     }
 }
